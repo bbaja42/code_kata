8db4d03cc2ceab7155090a3181a9a9e7a29fceff
Barisa Obradovic <barisa@intercom.io>
2016-03-03 08:39:54 +0000

    WIP

---
+++ meme.rb
@@ -0,0 +1,5 @@
+class Meme
+  def add(input)
+    0
+  end
+end
---
+++ meme_test.rb
@@ -0,0 +1,11 @@
+require "minitest/autorun"
+
+class TestMeme < Minitest::Test
+  def setup
+    @meme = Meme.new
+  end
+
+  def test_method_can_take_no_arguments
+    assert_equal 0, @meme.add("")
+  end
+end
